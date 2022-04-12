d5925e3dcf9a0505f170cd5fc55a43deec125ed6
devcb1313 <95555574+devcb1313@users.noreply.github.com>
2022-04-12 08:10:43 +0530

    lab

---
+++ Lab Cycles/Cycle 3/PygamePractice/space.py
@@ -0,0 +1,74 @@
+
+import pygame
+import os
+
+
+
+#CONSTANTS AND LOADING
+Wid,Hei = 900,500
+SPACESHIP_WIDTH,SPACESHIP_HEIGHT = 55,40
+VEL = 3
+WIN  = pygame.display.set_mode((Wid,Hei))
+pygame.display.set_caption("Game 1")
+COLOUR = (0,0,102)  #RGB colour values are stored here
+FPS = 60 #Framerate per second
+#Loading 2 images 
+#we use os so that the path is found
+SHIP_1 = pygame.image.load(os.path.join(
+    "Assets",'spaceship_yellow.png'))
+SHIP_2 = pygame.image.load(os.path.join(
+    "Assets",'spaceship_red.png'))
+#pygame.transform is used to rotate and scale the image
+SHIP_1 = pygame.transform.rotate(pygame.transform.scale(
+    SHIP_1,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),270)
+SHIP_2 = pygame.transform.rotate(pygame.transform.scale(
+    SHIP_2,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
+
+#Declaring the main surface
+def draw_window(red,yellow):
+    WIN.fill((COLOUR)) 
+    WIN.blit(SHIP_1,(yellow.x,yellow.y))
+    WIN.blit(SHIP_2,(red.x,red.y)) #Blit is used to put images and texts onto the screen
+    pygame.display.update()
+
+#to press the keys and define movements for red and yellow
+def yellow_handle_movement(keys_pressed,yellow):
+        if keys_pressed[pygame.K_a]: #LEFT
+            yellow.x-=VEL
+        if keys_pressed[pygame.K_d]: #RIGHT
+            yellow.x+=VEL
+        if keys_pressed[pygame.K_w]: #UP
+            yellow.y-=VEL
+        if keys_pressed[pygame.K_s]: #DOWN
+            yellow.y+=VEL
+
+def red_handle_movement(keys_pressed,red):
+        if keys_pressed[pygame.K_RIGHT]: #RIGHT
+            red.x+=VEL
+        if keys_pressed[pygame.K_UP]: #UP
+            red.y-=VEL
+        if keys_pressed[pygame.K_DOWN]: #DOWN
+            red.y+=VEL
+        if keys_pressed[pygame.K_LEFT]: #LEFT
+            red.x-=VEL
+
+def main():
+    red = pygame.Rect(675,200,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(225,200,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
+    clock = pygame.time.Clock() #run at 60FPS
+    run = True
+    while run:        #Ensures that the game keeps running
+        clock.tick(FPS)
+        i = 200
+        for event in pygame.event.get(): #Shows the event
+            if event.type == pygame.QUIT: #Quiting the application
+                run = False
+            draw_window(red,yellow)
+        keys_pressed = pygame.key.get_pressed() #returns the key getting pressed
+        yellow_handle_movement(keys_pressed,yellow)
+        red_handle_movement(keys_pressed,red)
+    pygame.quit()  #Quits the application
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lab Cycles/Cycle 3/PygamePractice/ss.py
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="qrichtext" content="1" /><style type="text/css">
+p, li { white-space: pre-wrap; }
+</style></head><body style=" font-family:'Ubuntu Mono'; font-size:17pt; font-weight:400; font-style:normal;">
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Python 3.8.10 (default, Mar 15 2022, 12:22:08)</p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Type &quot;copyright&quot;, &quot;credits&quot; or &quot;license&quot; for more information.</p>
+<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">IPython 7.13.0 -- An enhanced Interactive Python.</p>
+<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">In [</span><span style=" font-weight:600; color:#00ff00;">1</span><span style=" color:#00ff00;">]:</span> debugfile('/home/dev/c ++ programs2/Python-Lab/Lab Cycles/Cycle 3/PygamePractice/space.py', wdir='/home/dev/c ++ programs2/Python-Lab/Lab Cycles/Cycle 3/PygamePractice')</p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">&gt; <span style=" color:#00ff00;">/home/dev/c ++ programs2/Python-Lab/Lab Cycles/Cycle 3/PygamePractice/space.py</span>(1)<span style=" color:#4682b4;">&lt;module&gt;</span><span style=" color:#00bfff;">()</span></p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">----&gt; 1 from</span> lib2to3<span style=" color:#ffff00;">.</span>pgen2<span style=" color:#ffff00;">.</span>token <span style=" color:#00ff00;">import</span> COLON</p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">      2 import</span> pygame</p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">      3 import</span> os</p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">      4 </span></p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">      5 </span></p>
+<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
+<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">ipdb&gt; </p>
+<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">ipdb&gt; </p></body></html>
